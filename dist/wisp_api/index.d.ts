28bac4b877c9271f090e126996075a32bb6cb2f4
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-11-27 17:07:28 -0800

    Restructure

--- dist/wisp_api/index.d.ts
+++ dist/wisp_api/index.d.ts
@@ -1,20 +1,26 @@
-export type RequestTypes = "GET" | "POST" | "PUT" | "PATCH" | "DELETE";
-export type PaginationData = {
-    total: number;
-    count: number;
-    perPage: number;
-    currentPage: number;
-    totalPages: number;
-};
-export interface WispAPICore {
-    domain: string;
-    uuid: string;
-    token: string;
-    logger: any;
-}
-export declare class WispAPICore {
+import { AllocationsAPI } from "./apis/allocations.js";
+import { AuditLogsAPI } from "./apis/audit_log.js";
+import { BackupsAPI } from "./apis/backups.js";
+import { DatabasesAPI } from "./apis/databases.js";
+import { FastDLAPI } from "./apis/fastdl.js";
+import { FilesystemAPI } from "./apis/filesystem.js";
+import { ModsAPI } from "./apis/mods.js";
+import { SchedulesAPI } from "./apis/schedules.js";
+import { ServersAPI } from "./apis/servers.js";
+import { StartupAPI } from "./apis/startup.js";
+import { SubusersAPI } from "./apis/subusers.js";
+export declare class WispAPI {
+    private core;
+    Allocations: AllocationsAPI;
+    AuditLogs: AuditLogsAPI;
+    Backups: BackupsAPI;
+    Databases: DatabasesAPI;
+    FastDL: FastDLAPI;
+    Filesystem: FilesystemAPI;
+    Mods: ModsAPI;
+    Schedules: SchedulesAPI;
+    Servers: ServersAPI;
+    Startup: StartupAPI;
+    Subusers: SubusersAPI;
     constructor(domain: string, uuid: string, token: string, logger: any);
-    setUUID(uuid: string): void;
-    makeURL(path: string): string;
-    makeRequest(method: RequestTypes, path: string, data?: any): Promise<Response>;
 }
--- dist/wisp_api/index.js
+++ dist/wisp_api/index.js
@@ -1,66 +1,28 @@
-export class WispAPICore {
+import { WispAPICore } from "./apis/index.js";
+import { AllocationsAPI } from "./apis/allocations.js";
+import { AuditLogsAPI } from "./apis/audit_log.js";
+import { BackupsAPI } from "./apis/backups.js";
+import { DatabasesAPI } from "./apis/databases.js";
+import { FastDLAPI } from "./apis/fastdl.js";
+import { FilesystemAPI } from "./apis/filesystem.js";
+import { ModsAPI } from "./apis/mods.js";
+import { SchedulesAPI } from "./apis/schedules.js";
+import { ServersAPI } from "./apis/servers.js";
+import { StartupAPI } from "./apis/startup.js";
+import { SubusersAPI } from "./apis/subusers.js";
+export class WispAPI {
     constructor(domain, uuid, token, logger) {
-        this.domain = domain;
-        this.uuid = uuid;
-        this.token = token;
-        this.logger = logger;
-    }
-    setUUID(uuid) {
-        this.uuid = uuid;
-    }
-    makeURL(path) {
-        return `https://${this.domain}/api/client/servers/${this.uuid}/${path}`;
-    }
-    async makeRequest(method, path, data) {
-        let url = this.makeURL(path);
-        const headers = new Headers({
-            "Content-Type": "application/json",
-            "Accept": "application/vnd.wisp.v1+json",
-            "Authorization": `Bearer ${this.token}`,
-            "User-Agent": "WispJS (https://github.com/CFC-Servers/wispjs, 1.0.0)"
-        });
-        const request = async () => {
-            let response;
-            console.log(`${method} -> ${url}`);
-            switch (method) {
-                case "GET":
-                    if (data !== null) {
-                        const params = new URLSearchParams(data);
-                        const uri = new URL(url);
-                        uri.search = params.toString();
-                        url = uri.toString();
-                        console.log(`Updated GET URL: ${url}`);
-                    }
-                    response = await fetch(url, { method: "GET", headers: headers });
-                    break;
-                case "POST":
-                    data = JSON.stringify(data);
-                    response = await fetch(url, { method: "POST", headers: headers, body: data });
-                    break;
-                case "PUT":
-                    data = data ? JSON.stringify(data) : "";
-                    response = await fetch(url, { method: "PUT", headers: headers, body: data });
-                    break;
-                case "PATCH":
-                    data = JSON.stringify(data);
-                    response = await fetch(url, { method: "PATCH", headers: headers, body: data });
-                    break;
-                case "DELETE":
-                    response = await fetch(url, { method: "DELETE", headers: headers });
-                    break;
-                default:
-                    throw new Error(`Invalid method: ${method}`);
-            }
-            if (!response.ok) {
-                const status = response.status;
-                const statusText = response.statusText;
-                this.logger.error(`Request failed: ${method} -> ${url}: ${status} - ${statusText}`);
-                const text = await response.text();
-                this.logger.error(text);
-                throw new Error(`Request failed! Status: ${status} - ${statusText}`);
-            }
-            return response;
-        };
-        return await request();
+        this.core = new WispAPICore(domain, uuid, token, logger);
+        this.Allocations = new AllocationsAPI(this.core);
+        this.AuditLogs = new AuditLogsAPI(this.core);
+        this.Backups = new BackupsAPI(this.core);
+        this.Databases = new DatabasesAPI(this.core);
+        this.FastDL = new FastDLAPI(this.core);
+        this.Filesystem = new FilesystemAPI(this.core);
+        this.Mods = new ModsAPI(this.core);
+        this.Schedules = new SchedulesAPI(this.core);
+        this.Servers = new ServersAPI(this.core);
+        this.Startup = new StartupAPI(this.core);
+        this.Subusers = new SubusersAPI(this.core);
     }
 }
